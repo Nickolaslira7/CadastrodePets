72b19b3a72c66af84acf0863674a1bb6d23c5f11
Nickolas lira7 <125027693+Nickolaslira7@users.noreply.github.com>
2023-08-28 08:11:10 -0300

    Project: pets

---
+++ assets/js/cassio.js
@@ -0,0 +1,8 @@
+function comporPet(){
+    let tutor = document.getElementById ("tutorInput").value;
+    let namePet = document.getElementById("namePet").value;
+    let especie = document.getElementById("especie").value;
+    let fotinha = document.getElementById("fotinha").value;
+    let nascimento = document.getElementById("nascimento").value;
+
+}
