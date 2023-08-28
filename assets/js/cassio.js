3e5a96797614563c5308606278c7ba7f412ffeff
Nickolas lira7 <125027693+Nickolaslira7@users.noreply.github.com>
2023-08-28 09:34:11 -0300

    feat

--- assets/js/cassio.js
+++ assets/js/cassio.js
@@ -1,28 +1,60 @@
-const pets = new pet();
-
-function comporPet(){
-    let tutor = document.getElementById ("tutor").value;
-    let namePet = document.getElementById("namePet").value;
-    let especie = document.getElementById("especie").value;
-    let fotinha = document.getElementById("fotinha").value;
-    let nascimento = document.getElementById("nascimento").value;
-
-    const pet = new pet(tutor, namePet, especie, fotinha, nascimento, pets.ContId());
-    gameLibrary.add(pet);
-
+class Pet{
+    constructor(tutor, namePet, specie, photograph, Birthday){
+        this.tutor = tutor;
+        this.namePet = namePet;
+        this.specie = specie;
+        this.photograph = photograph;
+        this.Birthday = Birthday;
+        this.id = this.ContId();
+    }
+    ContId() {
+        let id = 1;
+        id++
+        return id
+    }
 }
 
+function composePet() {
+    let tutor = document.getElementById("tutorInput").value
+    let namePet = document.getElementById("namePet-Input").value
+    let specie = document.getElementById("species-Input").value
+    let photograph = document.getElementById("Photograph-Input").value
+    let Birthday = document.getElementById("Birthday-Input").value
 
+    const NewPet = new Pet(tutor, namePet, specie, photograph, Birthday, pet.ContId());
+    petList.Add(NewPet);
+}
 
+class PetList{
+    constructor(){
+        this.petArrey = [];
+    }
+    Add(parameter){
+        this.petArrey.push(parameter);
+    }
+}
 
-function InputEmpty() {
-    let tutor = document.getElementById("input-tutor").value;
-    let namePet = document.getElementById("input-namePet").value;
-    let especie = document.getElementById("input-especie").value;
-    let fotinha = document.getElementById("input-fotinha").value;
-    let nascimento = document.getElementById("input-nascimento").value;
+const petList = new PetList();
 
-    if (tutor == "" || namePet == "" || especie == "" || fotinha == "" || nascimento == "") {
+function RegisterPet() {
+     isAnyInputEmpty();
+    isURLValid();
+    if (!isAnyInputEmpty() && isURLValid()) {
+        composePet();
+        InputsClund();
+        
+    }
+    listHTML();
+}
+
+function isAnyInputEmpty() {
+    let tutor = document.getElementById("tutorInput").value
+    let namePet = document.getElementById("namePet-Input").value
+    let specie = document.getElementById("species-Input").value
+    let photograph = document.getElementById("Photograph-Input").value
+    let Birthday = document.getElementById("Birthday-Input").value
+
+    if (tutor == "" || namePet == "" || specie == "" || photograph == "" || Birthday == "") {
         sendMsg("Preencha todos os campos!", "error")
         return true
     } else {
@@ -30,3 +62,71 @@ function InputEmpty() {
         return false
     }
 }
+
+function sendMsg(msg, type) {
+    let msgDiv = document.getElementById("text");
+    msgDiv.innerHTML = '';
+
+    let msgToScreen = `<p class="${type}"> ${msg} </p>`
+
+    msgDiv.innerHTML = msgToScreen;
+    setTimeout(function () {
+        msgDiv.innerHTML = `<p class="${type}"></p>`;
+    }, 3000);
+
+}
+
+function InputsClund() {
+    document.getElementById("tutorInput").value = "";
+    document.getElementById("namePet-Input").value = "";
+    document.getElementById("species-Input").value = "";
+    document.getElementById("Photograph-Input").value = "";
+    document.getElementById("Birthday-Input").value = "";
+}
+
+function isURLValid() {
+    const url = document.getElementById("Photograph-Input").value;
+
+    if (url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+        return true;
+    } else {
+        sendMsg("A url da imagem está errada!", "error")
+        return false;
+    }
+}
+
+function ShowRegister() {
+    document.getElementById("main1").classList.remove("hidden")
+    document.getElementById("main2").classList.add("hidden")
+}
+function ShowPets() {
+    document.getElementById("main2").classList.remove("hidden")
+    document.getElementById("main1").classList.add("hidden")
+}
+
+function listHTML() {
+
+    const listHTML = document.getElementById("listPets");
+    listHTML.innerHTML = '';
+    let array = petList.petArrey;
+
+    array.forEach(pet => {
+        const petDiv = `<div class="petDetail" id="div${pet.id}">
+        <img src="${pet.photograph}" alt="${pet.namePet}">
+        <p><b>Tutor:</b> ${pet.tutor}</p> 
+        <p><b>Nome do pet:</b> ${pet.namePet}</p> 
+        <p><b>Espécie:</b> ${pet.specie}</p> 
+        <p><b>Data de nascimento:</b> ${pet.Birthday}</p>
+        <button onclick="deletepet(${pet.id})">Delete</button>
+        </div>`;
+        listHTML.innerHTML += petDiv;
+    })
+
+
+
+}
+
+
+const pet = new Pet();
+
+
