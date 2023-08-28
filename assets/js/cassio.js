cb2a95868cdca6cec463596615cf9356d2cc05bd
Nickolas lira7 <125027693+Nickolaslira7@users.noreply.github.com>
2023-08-28 08:25:47 -0300

    feat: inputs

--- assets/js/cassio.js
+++ assets/js/cassio.js
@@ -1,8 +1,32 @@
+const pets = new pet();
+
 function comporPet(){
-    let tutor = document.getElementById ("tutorInput").value;
+    let tutor = document.getElementById ("tutor").value;
     let namePet = document.getElementById("namePet").value;
     let especie = document.getElementById("especie").value;
     let fotinha = document.getElementById("fotinha").value;
     let nascimento = document.getElementById("nascimento").value;
 
+    const pet = new pet(tutor, namePet, especie, fotinha, nascimento, pets.ContId());
+    gameLibrary.add(pet);
+
+}
+
+
+
+
+function InputEmpty() {
+    let tutor = document.getElementById("input-tutor").value;
+    let namePet = document.getElementById("input-namePet").value;
+    let especie = document.getElementById("input-especie").value;
+    let fotinha = document.getElementById("input-fotinha").value;
+    let nascimento = document.getElementById("input-nascimento").value;
+
+    if (tutor == "" || namePet == "" || especie == "" || fotinha == "" || nascimento == "") {
+        sendMsg("Preencha todos os campos!", "error")
+        return true
+    } else {
+        sendMsg("Cadastrado com sucesso", "correct")
+        return false
+    }
 }
